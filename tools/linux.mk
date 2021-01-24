e8786c24f595fd379e650f581f21001b77234448
ysugimoto <sugimoto@wnotes.net>
2021-01-25 01:16:07 +0900

    add empty share directory

--- tools/linux.mk
+++ tools/linux.mk
@@ -3,3 +3,5 @@ linux: copy
 copy:
 	cp $(BUILD_DIR)/kakasi/include/* $(DEPS_DIR)/include/
 	cp $(BUILD_DIR)/kakasi/lib/libkakasi.a $(DEPS_DIR)/linux/
+	cp $(BUILD_DIR)/kakasi/share/kakasi/* $(DEPS_DIR)/share/
+	echo "package linux" > $(DEPS_DIR)/linux/vendor.go
